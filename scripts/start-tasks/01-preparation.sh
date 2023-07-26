b68160ccfaad82e21325fd0ebcab659b55cbef95
Allar Viik <allar@clarifiedsecurity.com>
2023-07-26 14:09:23 +0300

    Added a check for user id 1000

--- scripts/start-tasks/01-preparation.sh
+++ scripts/start-tasks/01-preparation.sh
@@ -2,10 +2,30 @@
 
 echo -n -e ${C_CYAN}
 
+# Creating required files to avoid errors
 touch ./container/home/builder/.zsh_history
 touch ./container/home/builder/.custom_aliases
 touch .makerc-custom
 touch .makerc-project
 touch .makerc-personal
+
+# Checking if user id equals 1000 on Linux
+if [[ $(uname) == "Linux" ]]; then
+
+    if [ "$(id -u)" -eq 1000 ]; then
+
+        echo -n -e
+
+    else
+
+        echo -e ${C_RED}
+        echo -e "Your user id is not 1000"
+        echo -e "Change your used id to 1000 or build the Catapult Docker images yourself with: "
+        echo -e "make build"
+        echo -e ${C_RST}
+
+    fi
+
+fi
 
 echo -n -e ${C_RST}
