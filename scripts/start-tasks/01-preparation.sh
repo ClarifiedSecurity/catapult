1ae42654356a548e467d122fba189466f3b5191e
Allar Viik <allar@clarifiedsecurity.com>
2023-07-31 09:00:05 +0300

    Added user id 1000 check to let know that non-1000 users need to build the image themselves

--- scripts/start-tasks/01-preparation.sh
+++ scripts/start-tasks/01-preparation.sh
@@ -8,23 +8,4 @@ touch ./container/home/builder/.custom_aliases
 touch .makerc-custom
 touch .makerc-personal
 
-# Checking if user id equals 1000 on Linux
-if [[ $(uname) == "Linux" ]]; then
-
-    if [ "$(id -u)" -eq 1000 ]; then
-
-        echo -n -e
-
-    else
-
-        echo -e ${C_RED}
-        echo -e "Your user id is not 1000"
-        echo -e "Change your used id to 1000 or build the Catapult Docker images yourself with: "
-        echo -e "make build"
-        echo -e ${C_RST}
-
-    fi
-
-fi
-
 echo -n -e ${C_RST}
