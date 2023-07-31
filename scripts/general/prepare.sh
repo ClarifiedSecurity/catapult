6ec2bff5c35fde99a28ade207490c07076a6b3f7
Allar Viik <allar@clarifiedsecurity.com>
2023-07-31 16:22:22 +0300

    Added a user prompt before installing the latest Docker version & updated `make prepare` colors

--- scripts/general/prepare.sh
+++ scripts/general/prepare.sh
@@ -2,14 +2,12 @@
 
 set -e # exit when any command fails
 
-echo -n -e ${C_MAGENTA}
-
 # Logging into Docker registry if it's defined
 docker-registry-login() {
 
 echo -n -e ${C_MAGENTA}
-
 echo -e "Using ${MAKEVAR_CONTAINER_REGISTRY} as Docker registry"
+echo -n -e ${C_RST}
 
 if [[ "${MAKEVAR_CONTAINER_REGISTRY_REQUIRES_AUTH}" != "false" ]]; then
 
@@ -30,20 +28,26 @@ fi
 
 if [[ $(uname) == "Darwin" ]]; then
 
-  echo "Configuring MacOS..."
-
-  echo "Installing MacOS packages with homebrew..."
+  echo -n -e ${C_MAGENTA}
+  echo -e "Configuring MacOS..."
+  echo -e "Installing MacOS packages with homebrew..."
+  echo -n -e ${C_RST}
   brew install git-lfs curl
 
 fi
 
 if [[ $(uname) == "Linux" ]]; then
 
-  echo "Configuring Linux..."
+  echo -n -e ${C_MAGENTA}
+  echo -e "Configuring Linux..."
+  echo -n -e ${C_RST}
 
   if grep -q "debian" /etc/os-release; then
 
-    echo "Installing required deb packages..."
+    echo -n -e ${C_MAGENTA}
+    echo -e "Installing required deb packages..."
+    echo -n -e ${C_RST}
+
     apt-get update
     apt-get install git-lfs curl make -y
 
@@ -56,28 +60,39 @@ if [[ $(uname) == "Linux" ]]; then
 
   elif grep -q "arch" /etc/os-release; then
 
-	echo "Installing required pacman packages..."
+  echo -n -e ${C_MAGENTA}
+	echo -e "Installing required pacman packages..."
+  echo -n -e ${C_RST}
+
 	pacman -S git git-lfs make curl --noconfirm
 
 
   else
 
-    echo "Unsupported OS"
-    echo "Please install git."
-    echo "Please install git LFS for your OS and initialize it."
-    echo "Please install curl"
-    echo "Please install jq"
+    echo -n -e ${C_YELLOW}
+    echo -e "Unsupported OS"
+    echo -e "Please install git."
+    echo -e "Please install git LFS for your OS and initialize it."
+    echo -e "Please install curl"
+    echo -e "Please install jq"
+    echo -n -e ${C_RST}
 
   fi
 
 fi
 
+echo -n -e ${C_MAGENTA}
 echo -e "Configuring githooks & LFS..."
+echo -n -e ${C_RST}
+
 touch ~/.gitconfig
 git config core.hooksPath .githooks
 git lfs install
 
+echo -n -e ${C_MAGENTA}
 echo -e "Fixing config files ownerships..."
+echo -n -e ${C_RST}
+
 chown $CONTAINER_USER_NAME ~/.gitconfig
 chown -R $CONTAINER_USER_NAME .githooks
 chown -R $CONTAINER_USER_NAME .git
@@ -85,6 +100,6 @@ chown -R $CONTAINER_USER_NAME .git
 scripts/general/configure-docker.sh
 docker-registry-login
 
+echo -n -e ${C_MAGENTA}
 echo "Preparation finished"
-
 echo -n -e ${C_RST}
