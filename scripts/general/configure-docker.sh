e792a32f258adee7cdd6a1c88f43c5380bc9bda0
Allar Viik <allar@clarifiedsecurity.com>
2023-08-01 09:24:49 +0300

    Updated docker installation scrit to allow advanced users to manually configure docker

--- scripts/general/configure-docker.sh
+++ scripts/general/configure-docker.sh
@@ -4,158 +4,146 @@ set -e # exit when any command fails
 
 install_docker(){
 
-if [[ $(uname) == "Linux" ]]; then
+  if [[ $(uname) == "Linux" ]]; then
 
-  if grep -q "debian" /etc/os-release; then
+    if grep -q "debian" /etc/os-release; then
 
-    if [[ -z $(grep -r download.docker.com /etc/apt/sources.list.d/) ]]; then
+      if [[ -z $(grep -r download.docker.com /etc/apt/sources.list.d/) ]]; then
 
-      echo -n -e ${C_MAGENTA}
-      echo "Adding Docker apt repo on Linux..."
-      echo -n -e ${C_RST}
+        if grep -q "Ubuntu" /etc/os-release; then
 
-      if grep -q "Ubuntu" /etc/os-release; then
+          echo -n -e ${C_MAGENTA}
+          echo "Adding Docker repo for $(lsb_release -cs)..."
+          echo -n -e ${C_RST}
 
-        echo -n -e ${C_MAGENTA}
-        echo "Adding Docker repo for $(lsb_release -cs)..."
-        echo -n -e ${C_RST}
+          curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
+          echo \
+          "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+          $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
-        echo \
-        "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+        elif grep -q "Kali" /etc/os-release; then
 
-      elif grep -q "Kali" /etc/os-release; then
+          echo -n -e ${C_MAGENTA}
+          echo "Adding Docker repo for $(lsb_release -cs)..."
+          echo -n -e ${C_RST}
 
-        echo -n -e ${C_MAGENTA}
-        echo "Adding Docker repo for $(lsb_release -cs)..."
-        echo -n -e ${C_RST}
+          curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg --yes
+          printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-        curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /etc/apt/trusted.gpg.d/docker-archive-keyring.gpg --yes
-        printf '%s\n' "deb https://download.docker.com/linux/debian bullseye stable" |  tee /etc/apt/sources.list.d/docker.list > /dev/null
+        elif grep -q "Debian" /etc/os-release; then
 
-      elif grep -q "Debian" /etc/os-release; then
+          echo -n -e ${C_MAGENTA}
+          echo "Adding Docker repo for $(lsb_release -cs)..."
+          echo -n -e ${C_RST}
 
-        echo -n -e ${C_MAGENTA}
-        echo "Adding Docker repo for $(lsb_release -cs)..."
-        echo -n -e ${C_RST}
+          curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
+          echo \
+          "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+          $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
 
-        curl -fsSL https://download.docker.com/linux/debian/gpg | gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg --yes
-        echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        fi
 
       fi
 
+    elif grep -q "arch" /etc/os-release; then
+
+      echo -n -e ${C_MAGENTA}
+      echo "Docker will be installed with pacman..."
+      echo -n -e ${C_RST}
+
+    else
+
+      echo -n -e ${C_YELLOW}
+      echo "Unsupported operating system, please install Docker for your OS, after installation run the following commands ( or similar ) to create the docker network with ipv6:"
+      echo "bash scripts/general/configure-docker.sh"
+      echo "systemctl start docker"
+      echo "systemctl reload docker"
+      echo "docker network create ${CONTAINER_NETWORK} --ipv6 --subnet ${CONTAINER_NETWORK_IPV6_SUBNET} --subnet ${CONTAINER_NETWORK_IPV4_SUBNET}"
+      echo -n -e ${C_RST}
+
     fi
 
-  elif grep -q "arch" /etc/os-release; then
+    # Installing Docker & required tools
+    if grep -q "debian" /etc/os-release; then
 
-    echo -n -e ${C_MAGENTA}
-    echo "Docker will be installed with pacman..."
-    echo -n -e ${C_RST}
+      apt-get update
+      apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin git-lfs curl
 
-  else
+    elif grep -q "arch" /etc/os-release; then
 
-    echo -n -e ${C_YELLOW}
-    echo "Unsupported operating system, please install Docker for your OS, after installation run the following commands ( or similar ) to create the docker network with ipv6:"
-    echo "bash scripts/general/configure-docker.sh"
-    echo "systemctl start docker"
-    echo "systemctl reload docker"
-    echo "docker network create ${CONTAINER_NETWORK} --ipv6 --subnet ${CONTAINER_NETWORK_IPV6_SUBNET} --subnet ${CONTAINER_NETWORK_IPV4_SUBNET}"
-    echo -n -e ${C_RST}
+      pacman -S docker docker-compose docker-buildx --noconfirm
+      systemctl enable docker.service
+      systemctl start docker.service
+      mkdir -p /etc/docker
 
-  fi
-
-  # Installing Docker & required tools
-  if grep -q "debian" /etc/os-release; then
-
-    apt-get update
-    apt-get -y install docker-ce docker-ce-cli containerd.io docker-compose-plugin git-lfs curl
-
-  elif grep -q "arch" /etc/os-release; then
-
-    pacman -S docker docker-compose docker-buildx --noconfirm
-    systemctl enable docker.service
-    systemctl start docker.service
-    mkdir -p /etc/docker
+    fi
 
   fi
-
-fi
-
-if [[ $(uname) == "Darwin" ]]; then
-
-  brew install --cask docker
-
-  # Wait until Docker is running
-  while ! docker ps >/dev/null 2>&1; do
-
-      # Launch Docker
-      echo "Waiting until Docker engine is running..."
-      open --background --hide -a Docker
-      sleep 5
-
-  done
-
-fi
-
-}
-
-echo -n -e ${C_MAGENTA}
-echo "Checking if ${CONTAINER_NETWORK} exists..."
-echo -n -e ${C_RST}
-
-if [[ -z $(docker network ls | grep ${CONTAINER_NETWORK}) ]]; then
-
-  echo -n -e ${C_MAGENTA}
-  echo "Creating Docker ${CONTAINER_NETWORK} network..."
-  echo -n -e ${C_RST}
-
-  docker network create ${CONTAINER_NETWORK} --ipv6 --subnet ${CONTAINER_NETWORK_IPV6_SUBNET} --subnet ${CONTAINER_NETWORK_IPV4_SUBNET}
-
-fi
-
-update_docker_config(){
 
   if [[ $(uname) == "Darwin" ]]; then
 
-    DOCKER_CONFIG_FILE="$HOME/.docker/daemon.json"
+    brew install --cask docker
+
+    # Wait until Docker is running
+    while ! docker ps >/dev/null 2>&1; do
+
+        # Launch Docker
+        echo "Waiting until Docker engine is running..."
+        open --background --hide -a Docker
+        sleep 5
+
+    done
+
+  fi
+
+}
+
+update_docker_config(){
+
+  if [[ -x "$(command -v docker)" ]]; then
 
     echo -n -e ${C_MAGENTA}
     echo "Updating Docker configuration..."
     echo -n -e ${C_RST}
 
-    echo $docker_config | jq > $DOCKER_CONFIG_FILE
+    if [[ $(uname) == "Darwin" ]]; then
 
-  else
-
-    DOCKER_CONFIG_FILE="/etc/docker/daemon.json"
-
-    # Only updating config if it's different
-    if [[ $(echo $docker_config | jq | sha1sum - | cut -d " " -f 1) != $(cat $DOCKER_CONFIG_FILE | sha1sum - | cut -d " " -f 1) ]]; then
-
-      echo -n -e ${C_MAGENTA}
-      echo "Updating Docker configuration..."
-      echo -n -e ${C_RST}
-
+      DOCKER_CONFIG_FILE="$HOME/.docker/daemon.json"
       echo $docker_config | jq > $DOCKER_CONFIG_FILE
 
-      echo -n -e ${C_MAGENTA}
-      echo "Restarting Docker service..."
-      echo -n -e ${C_RST}
+    else
 
-      systemctl restart docker
+      DOCKER_CONFIG_FILE="/etc/docker/daemon.json"
+
+      # Only updating config if it's different
+      if [[ $(echo $docker_config | jq | sha1sum - | cut -d " " -f 1) != $(cat $DOCKER_CONFIG_FILE | sha1sum - | cut -d " " -f 1) ]]; then
+
+        echo $docker_config | jq > $DOCKER_CONFIG_FILE
+
+        echo -n -e ${C_MAGENTA}
+        echo "Restarting Docker service..."
+        echo -n -e ${C_RST}
+
+        systemctl restart docker
+
+      fi
 
     fi
 
+  else
+
+    echo -n -e ${C_RED}
+    echo "Cannot update configuration, docker not found."
+    exit 1
+    echo -n -e ${C_RST}
+
   fi
 
-  echo -n -e ${C_MAGENTA}
-  echo "Testing IPv6 connectivity..."
-  echo -n -e ${C_RST}
+    echo -n -e ${C_MAGENTA}
+    echo "Testing IPv6 connectivity..."
+    echo -n -e ${C_RST}
 
-  docker run --rm -t busybox ping6 -c 1 google.com || true # At this point it's more informational
+    docker run --rm -t busybox ping6 -c 1 google.com || true # At this point it's more informational
 
 }
 
@@ -246,5 +234,31 @@ select option in "${options[@]}"; do
         2) echo -e "Not configuring Docker"; break;;
     esac
 done
+
+# Configuring Docker network if docker is installed
+if [[ -x "$(command -v docker)" ]]; then
+
+  echo -n -e ${C_MAGENTA}
+  echo "Checking if ${CONTAINER_NETWORK} exists..."
+  echo -n -e ${C_RST}
+
+  if [[ -z $(docker network ls | grep ${CONTAINER_NETWORK}) ]]; then
+
+    echo -n -e ${C_MAGENTA}
+    echo "Creating Docker ${CONTAINER_NETWORK} network..."
+    echo -n -e ${C_RST}
+
+    docker network create ${CONTAINER_NETWORK} --ipv6 --subnet ${CONTAINER_NETWORK_IPV6_SUBNET} --subnet ${CONTAINER_NETWORK_IPV4_SUBNET}
+
+  fi
+
+else
+
+  echo -n -e ${C_RED}
+  echo "Cannot create ${CONTAINER_NETWORK} network, docker not found."
+  exit 1
+  echo -n -e ${C_RST}
+
+fi
 
 echo -n -e ${C_RST}
