6c7061d029063ad9a0baed0825e6e92b556259cd
Allar Viik <allar@clarifiedsecurity.com>
2023-08-13 11:50:57 +0300

    Migrated .makerc-vars variable existance check to start tasks

--- scripts/start-tasks/01-preparation.sh
+++ scripts/start-tasks/01-preparation.sh
@@ -9,3 +9,68 @@ touch .makerc-custom
 touch .makerc-personal
 
 echo -n -e ${C_RST}
+
+# Checking if custom .makerc-vars.example exists and using it if it does
+if [ -r custom/.makerc-vars.example  ]
+then
+
+    example_vars_file=custom/.makerc-vars.example
+
+  else
+
+    example_vars_file=.makerc-vars.example
+
+fi
+
+# Checking if all of the required variables are present in .makerc-vars
+FILE_PATH=$example_vars_file
+
+# Array to store the variables
+variables=()
+
+# Flag to indicate if parsing is active
+parsing=false
+
+# Read the file line by line
+while IFS= read -r line; do
+  # Check if parsing is active
+  if [ "$parsing" = true ]; then
+    # Check if the line matches the end delimiter
+    if [[ "$line" == "# REQUIRED_END"* ]]; then
+      # Stop parsing
+      parsing=false
+    else
+      # Remove leading and trailing whitespace from the line
+      line="${line#"${line%%[![:space:]]*}"}"
+      line="${line%"${line##*[![:space:]]}"}"
+
+      # Exclude empty lines and lines starting with #
+      if [ -n "$line" ] && [[ ! "$line" =~ ^# ]]; then
+        # Extract the content until the first space
+        variable="${line%% *}"
+
+        # Add the variable to the array if the environment variable with the same name is empty
+        if [ -z "${!variable}" ]; then
+
+          variables+=("$variable")
+
+        fi
+      fi
+    fi
+  else
+    # Check if the line matches the start delimiter
+    if [[ "$line" == "# REQUIRED_START"* ]]; then
+      # Start parsing
+      parsing=true
+    fi
+  fi
+done < "$FILE_PATH"
+
+# Print the variables that are not set
+for variable in "${variables[@]}"; do
+
+  echo -n -e ${C_RED}
+  echo -e "$variable value missing in ${C_YELLOW}${ROOT_DIR}/.makerc-vars${C_RED}"
+  echo -n -e ${C_RST}
+
+done
