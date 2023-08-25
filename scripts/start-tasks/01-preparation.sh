33f92bea86698e29c670dc355a8c587ba732f63b
Allar Viik <allar@clarifiedsecurity.com>
2023-08-25 10:40:10 +0300

    Added a feature to create personalized shell aliases

--- scripts/start-tasks/01-preparation.sh
+++ scripts/start-tasks/01-preparation.sh
@@ -5,6 +5,7 @@ echo -n -e ${C_CYAN}
 # Creating required files to avoid errors
 touch ./container/home/builder/.zsh_history
 touch ./container/home/builder/.custom_aliases
+touch ./container/home/builder/.personal_aliases
 touch .makerc-custom
 touch .makerc-personal
 
