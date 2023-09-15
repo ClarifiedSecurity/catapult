07271e9c2225977874739d1db03c87dba4c7868e
Allar Viik <allar@clarifiedsecurity.com>
2023-09-15 18:18:13 +0300

    Revert to previous tag

--- scripts/entrypoints/02-clarified-core-version-check.sh
+++ scripts/entrypoints/02-clarified-core-version-check.sh
@@ -11,13 +11,13 @@ if ping -c 1 github.com &> /dev/null
 then
 
   # Checking for shared roles version if MANIFEST.json exists
-  if [[ -f "/srv/ansible/ansible_collections/nova/core/MANIFEST.json" ]]; then
+  if [[ -f "/srv/ansible/ansible_collections/clarified/core/MANIFEST.json" ]]; then
 
-    galaxy_local_version=$(cat /srv/ansible/ansible_collections/nova/core/MANIFEST.json | jq -r '.collection_info.version')
+    galaxy_local_version=$(cat /srv/ansible/ansible_collections/clarified/core/MANIFEST.json | jq -r '.collection_info.version')
 
   fi
 
-  galaxy_remote_version_row=$(curl https://raw.githubusercontent.com/novateams/nova.core/main/nova/core/galaxy.yml -s | grep "version:" | cut -d " " -f 2)
+  galaxy_remote_version_row=$(curl https://raw.githubusercontent.com/ClarifiedSecurity/clarified.core/main/clarified/core/galaxy.yml -s | grep "version:" | cut -d " " -f 2)
   galaxy_remote_version=$( echo $galaxy_remote_version_row | cut -d: -f2 | xargs )
   galaxy_local_version_patch=$( echo $galaxy_local_version | cut -d. -f3 )
   galaxy_remote_version_patch=$( echo $galaxy_remote_version | cut -d. -f3 )
@@ -25,10 +25,10 @@ then
   if [[ "$galaxy_local_version" != "$galaxy_remote_version" ]]; then
 
     echo -n -e ${C_YELLOW}
-    echo -e "${C_YELLOW}Local nova.core collection version:${C_RST}" $galaxy_local_version
-    echo -e "${C_YELLOW}Remote nova.core collection version:${C_RST}" $galaxy_remote_version
+    echo -e "${C_YELLOW}Local clarified.core collection version:${C_RST}" $galaxy_local_version
+    echo -e "${C_YELLOW}Remote clarified.core collection version:${C_RST}" $galaxy_remote_version
     echo -n -e ${C_YELLOW}
-    echo -e "Remote nova.core collection differs from local"
+    echo -e "Remote clarified.core collection differs from local"
     echo -e "Would you like to update now?"
     echo -n -e ${C_RST}
     options=(
@@ -50,13 +50,10 @@ then
     done
   fi
 
-  unset galaxy_local_version
-  unset galaxy_remote_version
-
 else
 
   echo -n -e ${C_RED}
-  echo -e Cannot check for nova.core version!
+  echo -e Cannot check for clarified.core version!
   echo -e github.com is not reachable
   echo -n -e ${C_RST}
 
