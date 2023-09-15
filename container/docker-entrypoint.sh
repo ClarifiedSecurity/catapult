3307e80a41495f8e9ec66930c96e2c304ef7b915
Allar Viik <allar@clarifiedsecurity.com>
2023-09-15 22:40:22 +0300

    Made sure that Catapult installs both `clarified.core` & `nova.core` collections

--- container/docker-entrypoint.sh
+++ container/docker-entrypoint.sh
@@ -23,12 +23,17 @@ if ls '/tmp' | grep -i ansible-keepass.sock -q; then
   echo -e "KeePass already open"
 
 else
+
   until ~/keepass-decrypt-check.py; do
+
     read -s -p "$(echo -e "Enter your KeePass password: ")" kppwd && export KPPWD=$kppwd
+
   done
+
   /home/builder/kpsock.py /home/builder/KPDB.kbdx --key /home/builder/KPDB.key --log kpsock.log --log-level WARNING --ttl 28800 &
   unset KPPWD
   sleep 1
+
 fi
 
 # Copying mounted certificates to the correct location and trusting them if they are present
