26adf94cf9c05b90e14e9e97d6228df6e9fe666b
Allar Viik <allar@clarifiedsecurity.com>
2024-02-23 18:36:03 +0200

    Separated tools & Python packages Docker image layers to keep them smaller

--- scripts/general/install-docker-image-python.sh
+++ scripts/general/install-docker-image-python.sh
@@ -3,7 +3,7 @@
 set -e # exit when any command fails
 
 apt update
-apt install -y curl sudo git zsh gcc
+apt install -y sudo git zsh gcc
 
 su - builder -c '
 set -e # exit when any command fails
@@ -36,19 +36,7 @@ $BUILDER_HOME/.fzf/install --key-bindings --completion --update-rc
 	echo "source /home/builder/.personal_aliases"
 } >> /etc/zsh/zshrc
 
-apt install -y ca-certificates gnupg rsync iputils-ping jq sshpass git-lfs software-properties-common unzip
-apt install -y iproute2 traceroute dnsutils netcat-openbsd vim # Development tools
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-apt update
-apt install -y nodejs
-corepack enable
-cd /srv
-yarn set version stable
-apt upgrade -y
-
-apt remove -y gcc software-properties-common gnupg
+apt remove -y gcc
 apt autoremove -y
 apt autoclean -y
 rm -rf /var/lib/apt/lists/*
