911b02681afdeb9cf84d096ad11d2ee9bceabdc9
Allar Viik <allar@clarifiedsecurity.com>
2024-02-23 18:09:53 +0200

    Linted `start-tasks-loader.sh` with shellcheck

--- scripts/general/start-tasks-loader.sh
+++ scripts/general/start-tasks-loader.sh
@@ -8,21 +8,21 @@ set -e # exit when any command fails
 # shellcheck disable=SC1091
 source ./scripts/general/colors.sh
 
-echo -n -e ${C_CYAN}
-
+echo -n -e "${C_CYAN}"
 # Checking if docker is installed
 if ! [ -x "$(command -v docker)" ]; then
-  echo -n -e ${C_RED}
-  echo -e "Docker not found did you run 'make prepare' first!"
-  exit 1
-  echo -n -e ${C_RST}
+  echo -n -e "${C_RED}"
+  echo -e "Docker not found did you run 'install.sh' first!"
+  echo -n -e "${C_RST}"
+  exit 0
+
 fi
 
 START_TASKS_FILES="scripts/start-tasks/*.sh"
 CUSTOM_START_TASKS_FILES="custom/start-tasks/*.sh"
 
 for custom_startfile in $CUSTOM_START_TASKS_FILES; do
-  if [ -f $custom_startfile ]; then
+  if [ -f "$custom_startfile" ]; then
     # Comment in the echo line below for better debugging
     # echo -e "\n Processing custom $custom_startfile...\n"
     $custom_startfile
@@ -30,7 +30,7 @@ for custom_startfile in $CUSTOM_START_TASKS_FILES; do
 done
 
 for startfile in $START_TASKS_FILES; do
-  if [ -f $startfile ]; then
+  if [ -f "$startfile" ]; then
     # Comment in the echo line below for better debugging
     # echo -e "\n Processing $startfile...\n"
     $startfile
