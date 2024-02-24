bfe0095e6946dfab6a02998b1cf3ac62e03535e9
Allar Viik <allar@clarifiedsecurity.com>
2024-02-25 00:27:03 +0200

    Installing nodejs to Docker image via binary archive to further decrease the it's size

--- scripts/general/install-docker-image-tools.sh
+++ scripts/general/install-docker-image-tools.sh
@@ -2,20 +2,39 @@
 
 set -e # exit when any command fails
 
+NODE_VERSION=v21.6.2
+
+# Detecting platform architecture
+if [[ "$(uname -m)" == "x86_64" ]]; then
+
+  OS_PLATFORM=linux-x64
+
+else
+
+  OS_PLATFORM="linux-arm64"
+
+fi
+
 apt update
-apt install -y curl ca-certificates gnupg rsync iputils-ping jq sshpass git-lfs software-properties-common unzip
-apt install -y iproute2 traceroute dnsutils netcat-openbsd vim # Development tools
-curl -fsSL https://deb.nodesource.com/gpgkey/nodesource-repo.gpg.key | gpg --dearmor -o /etc/apt/keyrings/nodesource.gpg
-NODE_MAJOR=20
-echo "deb [signed-by=/etc/apt/keyrings/nodesource.gpg] https://deb.nodesource.com/node_$NODE_MAJOR.x nodistro main" | tee /etc/apt/sources.list.d/nodesource.list
-apt update
-apt install -y nodejs
+apt install -y curl xz-utils # Reqired for Docker image creation
+apt install -y ca-certificates rsync iputils-ping jq sshpass git-lfs unzip # Required for general Ansible usage
+apt install -y iproute2 traceroute dnsutils netcat-openbsd vim # Extra development & debugging tools
+
+cd /tmp
+curl -s -O https://nodejs.org/download/release/$NODE_VERSION/node-$NODE_VERSION-$OS_PLATFORM.tar.xz
+mkdir -p /usr/local/lib/nodejs
+tar -xJvf node-$NODE_VERSION-$OS_PLATFORM.tar.xz -C /usr/local/lib/nodejs
+
+ln -s /usr/local/lib/nodejs/node-$NODE_VERSION-$OS_PLATFORM/bin/node /usr/bin/node
+ln -s /usr/local/lib/nodejs/node-$NODE_VERSION-$OS_PLATFORM/bin/npm /usr/bin/npm
+ln -s /usr/local/lib/nodejs/node-$NODE_VERSION-$OS_PLATFORM/bin/npx /usr/bin/npx
+ln -s /usr/local/lib/nodejs/node-$NODE_VERSION-$OS_PLATFORM/bin/corepack /usr/bin/corepack
+
 corepack enable
 cd /srv
-yarn set version stable
-apt upgrade -y
+npm install --global yarn
 
-apt remove -y software-properties-common gnupg
+apt upgrade -y
 apt autoremove -y
 apt autoclean -y
 
