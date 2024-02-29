92aa6c484fd4081b862fc2563a1dbe3102172978
Allar Viik <allar@clarifiedsecurity.com>
2024-02-29 23:23:40 +0200

    Added `nova.core` collection auto-update for when using Catapult for CI pipelines

--- scripts/entrypoints/02-nova-core-version-check.sh
+++ scripts/entrypoints/02-nova-core-version-check.sh
@@ -5,6 +5,12 @@ echo -n -e "${C_GREEN}"
 REQUIREMENTS_FILE="/srv/requirements/requirements_nova.yml"
 REMOTE_URL=$(cat $REQUIREMENTS_FILE | grep "version_check_url:" | awk '{print $2}')
 
+update_collection() {
+
+  ansible-galaxy collection install -r $REQUIREMENTS_FILE --force -p /srv/ansible
+
+}
+
 if curl github.com --connect-timeout 2 -s > /dev/null
 then
 
@@ -20,27 +26,36 @@ then
 
   if [[ "$galaxy_local_version" != "$galaxy_remote_version" ]]; then
 
-    echo -n -e "${C_YELLOW}"
-    echo -e "${C_YELLOW}Local nova.core collection version:${C_RST}" "$galaxy_local_version"
-    echo -e "${C_YELLOW}Remote nova.core collection version:${C_RST}" "$galaxy_remote_version"
-    echo -n -e "${C_YELLOW}"
-    echo -e "Remote nova.core collection differs from local"
-    echo -e "Would you like to update now?"
-    echo -n -e "${C_RST}"
-    options=(
-      "yes"
-      "no"
-    )
+    if [ "$COLLECTIONS_AUTO_UPDATE" == 1 ]; then
 
-    # shellcheck disable=SC2034
-    select option in "${options[@]}"; do
-    echo -n -e "${C_YELLOW}"
-        case "$REPLY" in
-            yes|y|1) ansible-galaxy collection install -r $REQUIREMENTS_FILE --force -p /srv/ansible; break;;
-            no|n|2) echo -e "Not updating now"; break;;
-        esac
-    echo -n -e "${C_RST}"
-    done
+      update_collection
+
+    else
+
+      echo -n -e "${C_YELLOW}"
+      echo -e "${C_YELLOW}Local nova.core collection version:${C_RST}" "$galaxy_local_version"
+      echo -e "${C_YELLOW}Remote nova.core collection version:${C_RST}" "$galaxy_remote_version"
+      echo -n -e "${C_YELLOW}"
+      echo -e "Remote nova.core collection differs from local"
+      echo -e "Would you like to update now?"
+      echo -n -e "${C_RST}"
+      options=(
+        "yes"
+        "no"
+      )
+
+      # shellcheck disable=SC2034
+      select option in "${options[@]}"; do
+      echo -n -e "${C_YELLOW}"
+          case "$REPLY" in
+              yes|y|1) update_collection; break;;
+              no|n|2) echo -e "Not updating now"; break;;
+          esac
+      echo -n -e "${C_RST}"
+      done
+
+    fi
+
   fi
 
   unset galaxy_local_version
