b4fdfd24a2d2ac2e38252a9d588e4a60c84ffa78
Allar Viik <allar@clarifiedsecurity.com>
2024-05-07 18:32:24 +0300

    Refactored `configure-docker.sh` to be faster and simpler

--- scripts/general/configure-docker.sh
+++ scripts/general/configure-docker.sh
@@ -14,9 +14,11 @@ install_docker(){
 
   if [[ $(uname) == "Linux" ]]; then
 
+    DAEMON_PATH=/etc/docker/daemon.json
+
     if grep -q -E "ID=(kali|debian|ubuntu)" /etc/os-release; then
 
-      if [[ -z $(grep -r download.docker.com /etc/apt/sources.list.d/) ]]; then
+      if ! grep -q -r "download.docker.com" /etc/apt/sources.list.d/; then
 
         if grep -q "ID=ubuntu" /etc/os-release; then
 
@@ -75,11 +77,11 @@ install_docker(){
       echo -e "You are using unsupported or untested (Linux) operating system. Catapult may still work if you install Docker manually"
       echo -e "You'll need to follow these steps:"
       echo -e
-      echo -e "1) Install "${C_YELLOW}"Docker engine and Docker compose plugin"${C_RED}" for your OS from: "${C_YELLOW}"https://docs.docker.com/engine/install"${C_RED}""
+      echo -e "1) Install ${C_YELLOW}Docker engine and Docker compose plugin${C_RED} for your OS from: ${C_YELLOW}https://docs.docker.com/engine/install${C_RED}"
       echo -e "1.1) If you don't find your OS in the link above look around in the internet there might be guides available on how to install Docker for your OS"
       echo -e
 
-      read -p "Once you have installed Docker & Docker compose plugin Press ENTER key to continue..."
+      read -rp "Once you have installed Docker & Docker compose plugin Press ENTER key to continue..."
       echo -n -e "${C_RST}"
 
     fi
@@ -111,6 +113,8 @@ install_docker(){
 
   if [[ $(uname) == "Darwin" ]]; then
 
+    DAEMON_PATH=$HOME/.docker/daemon.json
+
     if [[ -x "$(command -v brew)" ]]; then
 
       brew install --cask docker
@@ -136,120 +140,11 @@ install_docker(){
 
   fi
 
-}
+####################################
+# Docker daemon.json configuration #
+####################################
 
-#######################################
-# Docker daemon.json install function #
-#######################################
-update_docker_config(){
-
-  if [[ -x "$(command -v docker)" ]]; then
-
-    # Setting the correct daemon.json path
-    if [[ $(uname) == "Darwin" ]]; then
-
-      daemon_path=$HOME/.docker/daemon.json
-
-    else
-
-      daemon_path=/etc/docker/daemon.json
-
-    fi
-
-    if [[ $catapult_docker_mode == "advanced" ]]; then
-
-      echo -e "${C_YELLOW}"
-      echo -e "To disable IPtables and enable IPv6 make sure that the following parameters are in $daemon_path:"
-      echo -e
-      echo -e $docker_config | jq
-      echo -e "${C_YELLOW}"
-      read -p "Press ENTER key to continue"$'\n'
-
-    else
-
-      if [[ $(uname) == "Darwin" ]]; then
-
-        echo -e
-        echo -e "Overwriting your $daemon_path with the following config:"
-        echo -e
-
-      else
-
-        echo -e
-        echo -e "Overwriting your $daemon_path with the following config:"
-        echo -e
-
-      fi
-
-      echo $docker_config | jq
-
-      echo -e "${C_YELLOW}"
-      read -p "Press ENTER key to continue, or Ctrl + C to cancel and start over..."$'\n'
-      echo -e
-
-      echo -n -e "${C_MAGENTA}"
-      echo "Updating Docker configuration..."
-      echo -n -e "${C_RST}"
-
-      if [[ $(uname) == "Darwin" ]]; then
-
-        DOCKER_CONFIG_FILE="$HOME/.docker/daemon.json"
-
-      else
-
-        DOCKER_CONFIG_FILE="/etc/docker/daemon.json"
-
-      fi
-
-      # Restarting Docker service if DOCKER_CONFIG_FILE does not exist or hash is different
-      if [[ ! -f $DOCKER_CONFIG_FILE ]] || [[ $(echo $docker_config | jq | sha1sum - | cut -d " " -f 1) != $(cat "$DOCKER_CONFIG_FILE" | sha1sum - | cut -d " " -f 1) ]]; then
-
-        echo $docker_config | jq > $DOCKER_CONFIG_FILE
-
-        if [[ $(uname) == "Linux" ]]; then
-
-            echo -n -e "${C_MAGENTA}"
-            echo "Restarting Docker service..."
-            echo -n -e "${C_RST}"
-
-            systemctl restart docker
-
-        fi
-
-      fi
-
-    fi
-
-  else
-
-    echo -n -e "${C_RED}"
-    echo "Cannot update configuration, docker not found."
-    echo -n -e "${C_RST}"
-    exit 1
-
-  fi
-
-}
-
-echo -e "${C_YELLOW}"
-echo -e "Installing latest Docker version for your OS"
-echo -e
-
-options=(
-  "Yes it's fine"
-  "No, I'll manage my Docker version manually"
-)
-
-select option in "${options[@]}"; do
-    case "$REPLY" in
-        yes|y|1) install_docker; break;;
-        no|n|2) echo -e "Not installing Docker"; break;;
-    esac
-done
-
-echo -n -e "${C_RST}"
-
-docker_config_default=$(cat <<EOF
+DOCKER_CONFIG=$(cat <<EOF
 {
   "experimental": true,
   "ip6tables": true
@@ -257,28 +152,63 @@ docker_config_default=$(cat <<EOF
 EOF
 )
 
-docker_config_no_iptables=$(cat <<EOF
-{
-  "experimental": true,
-  "iptables": false,
-  "ip6tables": false
+echo -e "${C_MAGENTA}"
+echo -e "Updating ${C_CYAN}$DAEMON_PATH${C_MAGENTA} with:"
+echo -e "$DOCKER_CONFIG" | jq
+echo -e
+
+if [[ ! -f $DAEMON_PATH ]] || [[ ! -s $DAEMON_PATH ]]; then
+
+  echo "$DOCKER_CONFIG" | jq > "$DAEMON_PATH"
+
+  if [[ $(uname) == "Linux" ]]; then
+
+    echo -n -e "${C_MAGENTA}"
+    echo "Restarting Docker service..."
+    echo -n -e "${C_RST}"
+    systemctl restart docker
+
+  fi
+
+else
+
+  CURRENT_FILE_HASH=$(sha1sum < "$DAEMON_PATH")
+  jq '. + {"experimental": true, "ip6tables": true}' "$DAEMON_PATH" > /tmp/daemon.json
+  mv /tmp/daemon.json "$DAEMON_PATH"
+  UPDATED_FILE_HASH=$(sha1sum < "$DAEMON_PATH")
+
+  if [[ $CURRENT_FILE_HASH != "$UPDATED_FILE_HASH" ]]; then
+
+    echo -n -e "${C_MAGENTA}"
+    echo "Restarting Docker service..."
+    echo -n -e "${C_RST}"
+
+    if [[ $(uname) == "Linux" ]]; then
+
+      systemctl restart docker
+
+    fi
+
+  fi
+
+fi
+
 }
-EOF
-)
 
 echo -e "${C_YELLOW}"
-echo -e "Do you want Docker to automatically configure IPv4, IPv6 & manage it's IPtables?"
+echo -e "Installing and configuring the latest Docker version?"
 echo -e
 
 options=(
-  "Yes (recommended)"
-  "No I'll manage Docker config & IPTables myself (for advanced users)"
+  "Yes (recommended)."
+  "No, I'll manage my Docker version and configuration myself (for advanced users)."
 )
 
+# shellcheck disable=SC2034
 select option in "${options[@]}"; do
     case "$REPLY" in
-        yes|y|1) catapult_docker_mode=default docker_config=$docker_config_default update_docker_config; break;;
-        no|n|2) catapult_docker_mode=advanced docker_config=$docker_config_no_iptables update_docker_config; break;;
+        yes|y|1) install_docker; break;;
+        no|n|2) echo -e "Not installing and configuring Docker"; break;;
     esac
 done
 
