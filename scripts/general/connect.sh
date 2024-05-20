93b5a74ba165397e498796ead3ae513ab83bb7d6
Allar Viik <allar@clarifiedsecurity.com>
2024-05-20 15:34:48 +0300

    Exiting `ctp host connec <tab>` command if the playbook exists with an error

--- scripts/general/connect.sh
+++ scripts/general/connect.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e
+
 ansible-playbook /srv/inventories/start.yml -e @~/.vault/vlt -e=deploy_mode=deploy -e role_only=true -e connection_connect=true -e single_role=nova.core.connection -l "$@"
 # shellcheck disable=SC2046
 ssh $(cat /tmp/ansible_connect)
