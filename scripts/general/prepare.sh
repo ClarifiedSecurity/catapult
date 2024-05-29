264d846bc4f6f63a5f18447e86ff39e1f725cd24
Allar Viik <allar@clarifiedsecurity.com>
2024-05-29 17:13:06 +0300

    Added next stop info after running `make prepare`

--- scripts/general/prepare.sh
+++ scripts/general/prepare.sh
@@ -8,5 +8,6 @@ source ./scripts/general/colors.sh
 scripts/general/configure-docker.sh
 
 echo -n -e "${C_MAGENTA}"
-echo "Preparations finished successfully"
+echo -e "Preparations finished successfully"
+echo -e "Run ${C_CYAN}make start${C_MAGENTA} to start and configure Catapult"
 echo -n -e "${C_RST}"
