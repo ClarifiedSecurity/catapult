7a70a0a3fff29e9f1d7e214e2963ed9207dcea32
Allar Viik <allar@clarifiedsecurity.com>
2024-06-27 09:48:36 +0300

    Not executing first-run requirements installer anymore because custom requirements come from entrypoint scripts

--- scripts/entrypoints/01-initialization.sh
+++ scripts/entrypoints/01-initialization.sh
@@ -2,15 +2,6 @@
 
 echo -n -e "${C_GREEN}"
 
-# Chcking if ansible folder exists, if not then including requirements installer
-if [[ ! -d "/srv/ansible" ]]; then
-
-  echo -e "Running first-run requirements installer..."
-  # shellcheck disable=SC1091
-  source scripts/general/install-requirements.sh EXTRA
-
-fi
-
 # Installing or updating yarn npm packages
 # Using /dev/null because the --silent flag is not working for some reason anymore
 (cd /srv/ && yarn install > /dev/null)
