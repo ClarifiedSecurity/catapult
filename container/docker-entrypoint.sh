4c98e003c093dbe268a8f68b1f003c55acac3161
Allar Viik <allar@clarifiedsecurity.com>
2024-07-16 17:31:09 +0300

    Added a CATAPULT_SKIP_ENTRYPOINT env var to bypass initial docker-entrypoint.sh

--- container/docker-entrypoint.sh
+++ container/docker-entrypoint.sh
@@ -1,82 +1,90 @@
 #!/usr/bin/env bash
 
-# shellcheck disable=SC1091
-source /srv/scripts/general/colors.sh
+# Set CATAPULT_SKIP_ENTRYPOINT=1 to skip the entrypoint
+# Useful when using the image in CI
+if [ "$CATAPULT_SKIP_ENTRYPOINT" != 1 ]; then
 
-echo -n -e "${C_GREEN}"
+  # shellcheck disable=SC1091
+  source /srv/scripts/general/colors.sh
 
-# Activating Python virtual environment
-export PATH=$HOME/.cargo/bin:$PATH
-# shellcheck disable=SC1091
-source "$HOME/.venv/bin/activate"
+  echo -n -e "${C_GREEN}"
 
-# Making sure that /ssh-agent has the correct permissions, required mostly for MacOS
-sudo chown -R "$(id -u)":"$(id -g)" /ssh-agent
+  # Activating Python virtual environment
+  export PATH=$HOME/.cargo/bin:$PATH
+  # shellcheck disable=SC1091
+  source "$HOME/.venv/bin/activate"
 
-# Secrets unlocker script
-# Can also be used with ctp secrets unlock
-source /srv/scripts/general/secrets-unlock.sh
+  # Making sure that /ssh-agent has the correct permissions, required mostly for MacOS
+  sudo chown -R "$(id -u)":"$(id -g)" /ssh-agent
 
-# Checking if this is the first run
-# If so then skipping a bunch of steps for better performance
-if [ ! -f /tmp/first-run ]; then
+  # Secrets unlocker script
+  # Can also be used with ctp secrets unlock
+  # shellcheck disable=SC1091
+  source /srv/scripts/general/secrets-unlock.sh
 
-    # Creating first run file
-    touch /tmp/first-run
+  # Checking if this is the first run
+  # If so then skipping a bunch of steps for better performance
+  if [ ! -f /tmp/first-run ]; then
 
-    # Running connectivity checks
-    /srv/scripts/general/connectivity-checks.sh
+      # Creating first run file
+      touch /tmp/first-run
 
-    # Trusting custom certificates if they are present
-    if [[ -d "/srv/custom/certificates" && "$(ls -A /srv/custom/certificates)" ]]; then
+      # Running connectivity checks
+      /srv/scripts/general/connectivity-checks.sh
 
-      echo -e "${C_YELLOW}Trusting custom certificates...${C_RST}"
-      sudo rsync -ar /srv/custom/certificates/ /usr/local/share/ca-certificates/ --ignore-existing
-      touch /tmp/trust_extra_certificates
+      # Trusting custom certificates if they are present
+      if [[ -d "/srv/custom/certificates" && "$(ls -A /srv/custom/certificates)" ]]; then
 
-    fi
+        echo -e "${C_YELLOW}Trusting custom certificates...${C_RST}"
+        sudo rsync -ar /srv/custom/certificates/ /usr/local/share/ca-certificates/ --ignore-existing
+        touch /tmp/trust_extra_certificates
 
-    # Trusting personal certificates if they are present
-    if [[ -d "/srv/personal/certificates" && "$(ls -A /srv/personal/certificates)" ]]; then
-
-      echo -e "${C_YELLOW}Trusting personal certificates...${C_RST}"
-      sudo rsync -ar /srv/personal/certificates/ /usr/local/share/ca-certificates/ --ignore-existing
-      touch /tmp/trust_extra_certificates
-
-    fi
-
-    # Updating certificates if needed
-    if [[ -f /tmp/trust_extra_certificates ]]; then
-
-      sudo update-ca-certificates > /dev/null 2>/dev/null # To avoid false positive error rehash: warning: skipping ca-certificates.crt,it does not contain exactly one certificate or CRL
-
-    fi
-
-    DOCKER_CONTAINER_ENTRYPOINT_CUSTOM_FILES="/srv/custom/docker-entrypoints/*.sh"
-    for custom_entrypoint in $DOCKER_CONTAINER_ENTRYPOINT_CUSTOM_FILES; do
-      if [ -f "$custom_entrypoint" ]; then
-        # Comment in the echo line below for better debugging
-        # echo -e "\n Processing $custom_entrypoint...\n"
-        # shellcheck disable=SC1090
-        source "$custom_entrypoint"
       fi
-    done
 
-    DOCKER_CONTAINER_ENTRYPOINT_FILES="/srv/scripts/entrypoints/*.sh"
-    for entrypoint in $DOCKER_CONTAINER_ENTRYPOINT_FILES; do
-      if [ -f "$entrypoint" ]; then
-        # Comment in the echo line below for better debugging
-        # echo -e "\n Processing $entrypoint...\n"
-        # shellcheck disable=SC1090
-        source "$entrypoint"
+      # Trusting personal certificates if they are present
+      if [[ -d "/srv/personal/certificates" && "$(ls -A /srv/personal/certificates)" ]]; then
+
+        echo -e "${C_YELLOW}Trusting personal certificates...${C_RST}"
+        sudo rsync -ar /srv/personal/certificates/ /usr/local/share/ca-certificates/ --ignore-existing
+        touch /tmp/trust_extra_certificates
+
       fi
-    done
+
+      # Updating certificates if needed
+      if [[ -f /tmp/trust_extra_certificates ]]; then
+
+        sudo update-ca-certificates > /dev/null 2>/dev/null # To avoid false positive error rehash: warning: skipping ca-certificates.crt,it does not contain exactly one certificate or CRL
+
+      fi
+
+      DOCKER_CONTAINER_ENTRYPOINT_CUSTOM_FILES="/srv/custom/docker-entrypoints/*.sh"
+      for custom_entrypoint in $DOCKER_CONTAINER_ENTRYPOINT_CUSTOM_FILES; do
+        if [ -f "$custom_entrypoint" ]; then
+          # Comment in the echo line below for better debugging
+          # echo -e "\n Processing $custom_entrypoint...\n"
+          # shellcheck disable=SC1090
+          source "$custom_entrypoint"
+        fi
+      done
+
+      DOCKER_CONTAINER_ENTRYPOINT_FILES="/srv/scripts/entrypoints/*.sh"
+      for entrypoint in $DOCKER_CONTAINER_ENTRYPOINT_FILES; do
+        if [ -f "$entrypoint" ]; then
+          # Comment in the echo line below for better debugging
+          # echo -e "\n Processing $entrypoint...\n"
+          # shellcheck disable=SC1090
+          source "$entrypoint"
+        fi
+      done
+
+  fi
+
+  # Running inventory selection script
+  # shellcheck disable=SC1091
+  source /srv/scripts/general/select-inventory.sh
+
+  echo -n -e "${C_RST}"
 
 fi
-
-# Running inventory selection script
-source /srv/scripts/general/select-inventory.sh
-
-echo -n -e "${C_RST}"
 
 exec zsh
