32a967d3dd20684a6b9220c8d971d5f19ca35540
Allar Viik <allar@clarifiedsecurity.com>
2024-08-21 09:00:40 +0300

    Refactored `catapult-customizer.sh` to be more readable

--- scripts/general/catapult-customizer.sh
+++ scripts/general/catapult-customizer.sh
@@ -11,16 +11,13 @@ if [[ -z $CATAPULT_CUSTOMIZER_REPO_NO_OVERWRITE && $MAKEVAR_FREEZE_UPDATE != 1 ]
 
     # Cloning the customizer repo if it's set
     if [[ -z "${MAKEVAR_CATAPULT_CUSTOMIZER_REPO}" ]]; then
-
         echo -n -e
-
     else
 
         # Extracting git clone path from MAKEVAR_CATAPULT_CUSTOMIZER_REPO in case it contains a branch
         CATAPULT_CUSTOMIZER_REPO=$(echo "${MAKEVAR_CATAPULT_CUSTOMIZER_REPO}" | awk '{print $1}')
 
         if git ls-remote "$CATAPULT_CUSTOMIZER_REPO" &> /dev/null; then
-
             rm -rf custom # Removing the custom directory if it exists
             echo -n -e "${C_YELLOW}"
             echo -e "Cloning customizer repo..."
@@ -29,38 +26,25 @@ if [[ -z $CATAPULT_CUSTOMIZER_REPO_NO_OVERWRITE && $MAKEVAR_FREEZE_UPDATE != 1 ]
             git clone $MAKEVAR_CATAPULT_CUSTOMIZER_REPO -q --depth 1 custom
             rm -rf custom/.git*
         else
-
             echo -e "${C_RED}${MAKEVAR_CATAPULT_CUSTOMIZER_REPO} repository is not available.${C_RST}"
-
         fi
-
     fi
-
 else
-
     if [[ -n $CATAPULT_CUSTOMIZER_REPO_NO_OVERWRITE ]]; then
-
         echo -n -e "${C_GREEN}"
         echo -e "CATAPULT_CUSTOMIZER_REPO_NO_OVERWRITE variable is not set, overwriting custom folder"
         echo -n -e "${C_RST}"
-
     fi
-
 fi
-
 
 # Checking if custom start.yml if it exists
 if [ -f custom/start.yml ]; then
-
     echo -e "Using custom start.yml..."
-    rm -f inventories/start
+    rm -f inventories/start.yml
     cp -R custom/start.yml inventories/start.yml
-
   else
-
-    rm -f inventories/start
+    rm -f inventories/start.yml
     cp -R defaults/start.yml inventories/start.yml
-
 fi
 
 # Copying custom Docker compose file extension if it exists
