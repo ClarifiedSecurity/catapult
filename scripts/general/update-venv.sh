e78d9edfc341f153e5acd0218f5df40087b302f8
Allar Viik <allar@clarifiedsecurity.com>
2024-09-13 15:52:23 +0300

    Set `ansible-core` version to 2.17.4 and updated Python packages

--- scripts/general/update-venv.sh
+++ scripts/general/update-venv.sh
@@ -1,8 +1,15 @@
 #!/usr/bin/env bash
 
-rm -rf $HOME/.venv
-pushd $HOME; $HOME/.cargo/bin/uv venv; popd
-source $HOME/.venv/bin/activate
-$HOME/.cargo/bin/uv pip install -r /srv/defaults/requirements_src.txt
+# shellcheck disable=SC1091
+source /srv/scripts/general/colors.sh
+
+rm -rf "$HOME/.venv"
+# shellcheck disable=SC2164
+pushd "$HOME"; "$HOME/.cargo/bin/uv" venv; popd
+source "$HOME/.venv/bin/activate"
+"$HOME/.cargo/bin/uv" pip install -r /srv/defaults/requirements_src.txt
 uv pip freeze > /srv/defaults/requirements.txt
-echo "Versioned requirements written into /srv/defaults/requirements.txt"
+
+echo -n -e "${C_YELLOW}"
+echo -e "Versioned requirements written into /srv/defaults/requirements.txt"
+echo -n -e "${C_RST}"
