58ad3238f581b022c4a1bd465a4a2f923638aaf8
Allar Viik <allar@clarifiedsecurity.com>
2024-09-13 15:53:59 +0300

    Running `make customizations` inside other make tasks so vars for them would already be loaded

--- scripts/general/prepare.sh
+++ scripts/general/prepare.sh
@@ -5,6 +5,10 @@ set -e # exit when any command fails
 # shellcheck disable=SC1091
 source ./scripts/general/colors.sh
 
+echo -n -e "${C_YELLOW}"
+make customizations --no-print-directory
+echo -n -e "${C_RST}"
+
 scripts/general/configure-docker.sh
 
 echo -n -e "${C_GREEN}"
