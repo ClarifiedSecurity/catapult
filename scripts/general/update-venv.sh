278af07274055ff52c788d4699d6fd5714e3582d
Allar Viik <allar@clarifiedsecurity.com>
2024-11-19 12:23:26 +0200

    Fixed the `ctp update venv` command to work with the latest uv

--- scripts/general/update-venv.sh
+++ scripts/general/update-venv.sh
@@ -4,9 +4,9 @@
 source /srv/scripts/general/colors.sh
 
 rm -rf "$HOME/.venv"
-pushd "$HOME" || exit ; "$HOME/.cargo/bin/uv" venv ; popd || exit
+pushd "$HOME" || exit ; "$HOME/.local/bin/uv" venv ; popd || exit
 source "$HOME/.venv/bin/activate"
-"$HOME/.cargo/bin/uv" pip install -r /srv/defaults/requirements_src.txt
+"$HOME/.local/bin/uv" pip install -r /srv/defaults/requirements_src.txt
 uv pip freeze > /srv/defaults/requirements.txt
 
 echo -n -e "${C_YELLOW}"
