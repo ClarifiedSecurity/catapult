0b1cdede68e98e69bb524cf79007df807c02e627
Allar Viik <allar@clarifiedsecurity.com>
2024-12-03 12:28:18 +0200

    Set Ansible version to 2.18.1 and updated Python packages

--- scripts/general/update-venv.sh
+++ scripts/general/update-venv.sh
@@ -7,7 +7,7 @@ rm -rf "$HOME/.venv"
 pushd "$HOME" || exit ; "$HOME/.local/bin/uv" venv ; popd || exit
 source "$HOME/.venv/bin/activate"
 "$HOME/.local/bin/uv" pip install -r /srv/defaults/requirements_src.txt
-uv pip freeze > /srv/defaults/requirements.txt
+$HOME/.local/bin/uv pip freeze > /srv/defaults/requirements.txt
 
 echo -n -e "${C_YELLOW}"
 echo -e "Versioned requirements written into /srv/defaults/requirements.txt"
