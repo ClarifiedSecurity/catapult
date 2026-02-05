55a75b0188b98ad0d73b352680a64ea52c5cac07
Allar Viik <allar@clarifiedsecurity.com>
2026-02-05 10:42:20 +0200

    Not overwriting Catapult customizer repo if the same version exists

--- scripts/general/catapult-customizer.sh
+++ scripts/general/catapult-customizer.sh
@@ -12,17 +12,30 @@ if [[ -z ${CATAPULT_CUSTOMIZER_REPO_NO_OVERWRITE} && ${MAKEVAR_FREEZE_UPDATE} ==
     # Cloning the customizer repo if it's set
     if [[ -n "${MAKEVAR_CATAPULT_CUSTOMIZER_REPO}" ]]; then
 
-        # Extracting git clone path from MAKEVAR_CATAPULT_CUSTOMIZER_REPO in case it contains a branch
-        CATAPULT_CUSTOMIZER_REPO=$(echo "${MAKEVAR_CATAPULT_CUSTOMIZER_REPO}" | awk '{print $1}')
+        if [[ -f custom/checksum ]]; then
+            CATAPULT_CUSTOMIZER_LOCAL_VERSION=$(cat custom/checksum)
+        fi
+        CATAPULT_CUSTOMIZER_REMOTE_VERSION=$(git ls-remote "${MAKEVAR_CATAPULT_CUSTOMIZER_REPO}" \
+        refs/heads/"${MAKEVAR_CATAPULT_CUSTOMIZER_VERSION}" 2>/dev/null | awk '{print $1}' | cut -c 1-7)
 
-        if git ls-remote "$CATAPULT_CUSTOMIZER_REPO" &> /dev/null; then
-            rm -rf custom # Removing the custom directory if it exists
-            echo -n -e "${C_YELLOW}"
-            echo -e "Cloning customizer repo ${C_CYAN}${MAKEVAR_CATAPULT_CUSTOMIZER_VERSION}${C_YELLOW} version..."
-            echo -n -e "${C_RST}"
-            # shellcheck disable=SC2086
-            git clone $MAKEVAR_CATAPULT_CUSTOMIZER_REPO -b ${MAKEVAR_CATAPULT_CUSTOMIZER_VERSION} -q --depth 1 custom
-            rm -rf custom/.git*
+        echo -n -e "${C_YELLOW}"
+        echo -e "Checking for Catapult Customizer updates..."
+        echo -n -e "${C_RST}"
+        if [[ -n "$CATAPULT_CUSTOMIZER_REMOTE_VERSION" ]]; then
+
+
+            if [[ "$CATAPULT_CUSTOMIZER_LOCAL_VERSION" != "$CATAPULT_CUSTOMIZER_REMOTE_VERSION" ]]; then
+
+                rm -rf custom # Removing the custom directory if it exists
+                echo -n -e "${C_YELLOW}"
+                echo -e "Cloning customizer repo ${C_CYAN}${MAKEVAR_CATAPULT_CUSTOMIZER_VERSION}${C_YELLOW} version..."
+                echo -n -e "${C_RST}"
+                # shellcheck disable=SC2086
+                git clone $MAKEVAR_CATAPULT_CUSTOMIZER_REPO -b ${MAKEVAR_CATAPULT_CUSTOMIZER_VERSION} -q --depth 1 custom
+                git -C custom rev-parse --short HEAD > custom/checksum
+                rm -rf custom/.git*
+            fi
+
         else
             echo -e "${C_RED}${MAKEVAR_CATAPULT_CUSTOMIZER_REPO} repository is not available!${C_RST}"
         fi
