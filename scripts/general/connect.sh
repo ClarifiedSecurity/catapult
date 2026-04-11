0ef81c5d35f28ea61fd8c70d8b8405ca62bde76d
Allar Viik <allar@clarifiedsecurity.com>
2026-04-11 12:20:21 +0300

    Redesigned `ctp host connect` to support sshpass

--- scripts/general/connect.sh
+++ scripts/general/connect.sh
@@ -4,4 +4,4 @@ set -e
 
 ansible-playbook "$PWD/playbook.yml" -e @~/.vault/vlt -e=deploy_mode=deploy -e role_only=true -e connection_connect=true -e single_role=nova.core.connection -l "$@"
 # shellcheck disable=SC2046
-ssh $(cat /tmp/ansible_connect)
+bash /tmp/ansible_connect
