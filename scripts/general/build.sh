bd68f4f4ef82a8ae5a05e757444dd9c45cea19ed
Allar Viik <allar@clarifiedsecurity.com>
2026-05-25 09:24:25 +0300

    Running cleanup after local Catapult image build

--- scripts/general/build.sh
+++ scripts/general/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-${MAKEVAR_SUDO_COMMAND} docker --context default buildx create --use --driver-opt network=host
-${MAKEVAR_SUDO_COMMAND} docker --context default buildx build ${BUILD_ARGS} --network host --progress plain --tag "${IMAGE_FULL}" . --load
+${MAKEVAR_SUDO_COMMAND} docker --context default buildx create --name catapult_builder --use --driver-opt network=host
+${MAKEVAR_SUDO_COMMAND} docker --context default buildx build ${BUILD_ARGS} --tag "${IMAGE_FULL}" . --load
+${MAKEVAR_SUDO_COMMAND} docker --context default buildx stop catapult_builder
+${MAKEVAR_SUDO_COMMAND} docker --context default buildx rm catapult_builder
