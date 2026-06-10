3258cc09568878355c040de9282f07833cd7ad3b
Allar Viik <allar@clarifiedsecurity.com>
2026-06-10 21:12:47 +0300

    Updated Python packages

--- scripts/general/update-venv.sh
+++ scripts/general/update-venv.sh
@@ -3,9 +3,14 @@
 # shellcheck disable=SC1091
 source /srv/scripts/general/colors.sh
 
+# Not using /tmp because it is mounted with noexec, which prevents uv from running.
+mkdir -p ~/.cache/tmp
+TMPDIR=~/.cache/tmp uv self update
+
 cp /srv/defaults/pyproject.toml "$HOME/catapult-venv/pyproject.toml"
 uv sync --upgrade --project "$HOME/catapult-venv"
 cp "$HOME/catapult-venv/uv.lock" /srv/defaults/uv.lock
+rm -rf ~/.cache/tmp
 
 echo -n -e "${C_YELLOW}"
 echo -e "Versioned requirements written into /srv/defaults/uv.lock"
